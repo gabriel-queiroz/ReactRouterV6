3ecca3a0ece5afd0f6c723d10b98d9073d23d4a1
gabriel-queirozn <64821297+gabriel-queirozn@users.noreply.github.com>
2022-01-15 18:42:36 -0300

    routes and auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
 import React from "react";
+import Routes from "./routes";
+import { Link } from "react-router-dom";
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/login">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/details">Details</Link>
+          </li>
+        </ul>
+        <hr />
+        <Routes />
+      </div>
+    </div>
+  );
 }
 
 export default App;
